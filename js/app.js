8e5fc7037504b83e8657d27c7979b778e8fcff87
Nieminen Ilari (Samlink External) <K843903@samlink.saminet.net>
2016-09-01 16:42:57 +0300

    initial commit

---
+++ js/app.js
@@ -0,0 +1,78 @@
+var app = angular.module('AllasApp', ['ngRoute']);
+
+
+app.config(function($routeProvider){
+  $routeProvider
+    .when('/', {
+      controller: 'AllasController',
+      templateUrl: 'views/etusivu.html',
+      activelink: ''
+    })
+    .when('/palvelut',{
+      controller: 'AllasController',
+      templateUrl: 'views/palvelut.html',
+      activelink: 'services'
+    })
+    .when('/yhteystiedot', {
+      controller: 'AllasController',
+      templateUrl: 'views/yhteystiedot.html',
+      activelink: 'contact'
+    })
+    .when('/galleria', {
+      controller: 'AllasController',
+      templateUrl: 'views/galleria.html',
+      activelink: 'gallery'
+    })
+    .when('/suositukset', {
+      controller: 'AllasController',
+      templateUrl: 'views/suositukset.html',
+      activelink: 'references'
+    })
+    .otherwise({
+      redirectTo: '/',
+      activelink: ''
+    });
+});
+
+app.controller('AllasController', function($scope, $route){
+  $scope.$route = $route;
+});
+
+
+
+
+$("#frontpagelink").click(function(){
+  $("#frontpage").show();
+  $("#gallery").hide();
+  $("#services").hide();
+  $("#contacts").hide();
+  $("#references").hide();
+});
+$("#gallerylink").click(function(){
+  $("#frontpage").hide();
+  $("#gallery").show();
+  $("#services").hide();
+  $("#contacts").hide();
+  $("#references").hide();
+});
+$("#servicelink").click(function(){
+  $("#frontpage").hide();
+  $("#gallery").hide();
+  $("#services").show();
+  $("#contacts").hide();
+  $("#references").hide();
+});
+$("#contactslink").click(function(){
+  $("#frontpage").hide();
+  $("#gallery").hide();
+  $("#services").hide();
+  $("#contacts").show();
+  $("#references").hide();
+});
+$("#referenceslink").click(function(){
+  $("#frontpage").hide();
+  $("#gallery").hide();
+  $("#services").hide();
+  $("#contacts").hide();
+  $("#references").show();
+});
