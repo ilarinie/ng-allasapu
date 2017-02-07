39d31d7e0cf58e36d436102691ffd4f09fe099fd
ilarinie <ilari.nieminen@outlook.com>
2017-02-07 11:19:10 +0200

    m

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = angular.module('app', ['ngRoute']);
 app.config(function($routeProvider) {
     $routeProvider
         .when("/", {
-            templateUrl: '/views/etusivu.html'
+            templateUrl: 'views/etusivu.html'
         })
         .when('/yhteystiedot', {
             templateUrl: 'views/yhteystiedot.html'
